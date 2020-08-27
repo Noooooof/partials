13defdd461575d97dcd05d205f8cab3252f401d9
Noooooof <Black1250889060@gmail.com>
2020-08-27 21:25:45 +0800

    easy-update

--- Release/update.cmd
+++ Release/update.cmd
@@ -1,4 +1,4 @@
 @echo off
 E:
 cd E:\Recent\MyBlog
-hexo clean && hexo g && hexo d && cd update && python deploy.py
+hexo clean && hexo g && hexo d && cd E:\Recent\MyBlog\source\_partials\Release && python deploy.py
