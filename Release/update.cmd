59dd90e13e7f22e4ebc0093e4c03d2e90846701b
Noooooof <Black1250889060@gmail.com>
2020-08-29 20:20:17 +0800

    easy-update

--- Release/update.cmd
+++ Release/update.cmd
@@ -3,7 +3,7 @@ E:
 cd E:\Recent\MyBlog\source\_partials\Image\Wordcloud
 python oper.py
 cd E:\Recent\MyBlog\source\_partials
-update.cmd
+call update.cmd
 cd E:\Recent\MyBlog
 hexo clean && hexo g && hexo d
 cd E:\Recent\MyBlog\source\_partials\Release
