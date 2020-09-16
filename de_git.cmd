950c9d57744c7e54db8ae6049a4d33b82691680c
Noooooof <Black1250889060@gmail.com>
2020-09-16 21:03:32 +0800

    easy-update

--- de_git.cmd
+++ de_git.cmd
@@ -1,9 +1,4 @@
 @echo off
-E:
-cd E:\Recent\MyBlog\source\_partials\Font\fontspider
-call spider.cmd
-Xcopy E:\Recent\MyBlog\source\_partials\Font\H-TTF-BuMing-R-2.ttf E:\Recent\MyBlog\themes\hexo-theme-white-master\source\font /Y
-cd E:\Recent\MyBlog\source\_partials
 git add .
 git status -s
 git commit -m "easy-update"
