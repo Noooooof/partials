decaf61c24521c56705d52bfe1d3ad37aebba366
Noooooof <Black1250889060@gmail.com>
2020-09-16 18:54:42 +0800

    easy-update

---
+++ Font/fontspider/spider.cmd
@@ -0,0 +1,4 @@
+@echo off
+python word.py
+Xcopy E:\Recent\MyBlog\source\_partials\Font\.font-spider\H-TTF-BuMing-R-2.ttf E:\Recent\MyBlog\source\_partials\Font /Y
+font-spider word.html
--- de_git.cmd
+++ de_git.cmd
@@ -1,7 +1,7 @@
 @echo off
 E:
-cd E:\Recent\MyBlog\source\_partials\Image\Wordcloud
-python oper.py
+cd E:\Recent\MyBlog\source\_partials\Font\fontspider
+call spider.cmd
 cd E:\Recent\MyBlog\source\_partials
 git add .
 git status -s
