80695156308c904c03b427ef4803abe8cbc5e15f
Noooooof <Black1250889060@gmail.com>
2020-09-16 22:23:32 +0800

    easy-update

--- de_git.cmd
+++ de_git.cmd
@@ -1,4 +1,7 @@
 @echo off
+E:
+rd E:\Recent\MyBlog\source\_partials\Theme /s /q
+xcopy E:\Recent\MyBlog\themes\hexo-theme-white-master E:\Recent\MyBlog\source\_partials\Theme\ /s /e /q
 git add .
 git status -s
 git commit -m "easy-update"
