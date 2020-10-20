ed0bf97f2f2970552515e9d5b3a73f775a59e93e
Noooooof <Black1250889060@gmail.com>
2020-10-20 23:59:31 +0800

    easy-update

--- Blog_update/update.cmd
+++ Blog_update/update.cmd
@@ -1,5 +1,6 @@
 @echo off
 E:
+cd E:\Recent\MyBlog\source\_update && python updated.py
 cd E:\Recent\MyBlog
 start chrome.exe https://gitee.com/Noooooof/Noooooof/pages
 hexo clean && hexo g && hexo d
