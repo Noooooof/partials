a880b118d0199a905caeefd63c9bc4ec90d3bb34
Noooooof <Black1250889060@gmail.com>
2020-11-16 01:28:04 +0800

    easy-update

--- Blog_update/updated.py
+++ Blog_update/updated.py
@@ -4,31 +4,50 @@ import os
 import re
 import time
 
-path = '../_posts' # _posts文件夹位置
+postpath = '../_posts' # _posts文件夹位置
 
-for file in os.listdir(path):
-	if ('.md' in file):
-		fileName = os.path.join(path,file)
-		with open(fileName,'r',encoding="utf-8")as fi:
-			postContent = fi.read() # markdown文件内容
+def oper(tag,fileName):
 
-		localDate = time.strftime("%Y-%m-%d", time.localtime()) # 当前时间
-		localUpdate = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(os.stat(fileName).st_mtime)) # 文件修改时间
-		
-		postCategoris = re.findall(r'categories: ([\s\S]*?)\n',postContent)[0] # 分类
-		postDate = re.findall(r'date: ([\s\S]*?)\n',postContent)[0] # date时间
-		postUpdate = re.findall(r'updated: ([\s\S]*?)\n',postContent)[0] # update时间
+	with open(fileName,'r',encoding="utf-8")as fi:
+		postContent = fi.read() # markdown文件内容
 
-		post = "" # 准备覆写
+	post = postContent # 准备覆写
+	flag = 0
 
-		if (localDate not in localUpdate): # 过往文章
-			if (postDate != postUpdate):
-				post = re.sub(r'updated: [\s\S]*?\n','updated: '+ postDate +'\n', postContent) # 按date时间复位
-		else:
-			if (localDate in postUpdate): # 今日更新文章
-				if ((postCategoris != '\'Diary\'') and (postDate != postUpdate)):
-					post = re.sub(r'date: [\s\S]*?\n','date: '+ postUpdate +'\n', postContent) # 连载按update时间更新date时间
-		
-		if (post):
-			with open(fileName,'w',encoding="utf-8")as fo:
-				fo.write(post) # 覆写
+	postCategoris = re.findall(r'categories: ([\s\S]*?)\n',postContent)[0] # 分类
+	if (postCategoris!=tag):
+		flag = 1
+		post = re.sub(r'categories: ([\s\S]*?)\n','categories: ' + tag + '\n',post)
+
+	localDate = time.strftime("%Y-%m-%d", time.localtime()) # 当前时间
+	postDate = re.findall(r'date: ([\s\S]*?)\n',postContent)[0] # date时间
+	postUpdate = re.findall(r'updated: ([\s\S]*?)\n',postContent)[0] # update时间
+	if (postDate != postUpdate):
+		if (localDate not in postUpdate): # 非new
+			flag = 1
+			post = re.sub(r'updated: [\s\S]*?\n','updated: '+ postDate +'\n', post) # 按date时间复位
+		else: # new post
+			if (tag != 'Diary'):
+				flag = 1
+				post = re.sub(r'date: [\s\S]*?\n','date: '+ postUpdate +'\n', post) # 连载按update时间更新date时间
+	
+	if (flag):
+		with open(fileName,'w',encoding="utf-8")as fo:
+			fo.write(post) # 覆写
+
+	return
+
+def finddir(tag,path):
+	for file in os.listdir(path):
+
+		filepath = os.path.join(path,file)
+
+		if (re.search(r'^[\._]',file)==None):
+			if os.path.isdir(filepath):
+				finddir(file,filepath)
+			else:
+				if (re.search(r'\.md$',file)!=None):
+					oper(tag,filepath)
+	return
+
+finddir('post',postpath)
