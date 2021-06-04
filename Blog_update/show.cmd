366e215eea635f39ed1322f3c04bc8ea6793461c
Noooooof <Black1250889060@gmail.com>
2021-06-04 17:50:36 +0800

    easy-update

--- Blog_update/show.cmd
+++ Blog_update/show.cmd
@@ -1,6 +1,6 @@
 @echo off
 E:
-start C:\Users\Black\AppData\Local\Google\Chrome\Application\chrome.exe http://localhost:4000
+start Chrome.exe http://localhost:4000
 cd E:\Recent\MyBlog\source\_update && python updated.py
 cd E:\Recent\MyBlog
 hexo clean && hexo g && hexo s
--- Blog_update/update.cmd
+++ Blog_update/update.cmd
@@ -2,4 +2,4 @@
 E:
 cd E:\Recent\MyBlog\source\_update && python updated.py
 cd E:\Recent\MyBlog
-hexo clean && hexo g && hexo d && start C:\Users\Black\AppData\Local\Google\Chrome\Application\chrome.exe https://gitee.com/Noooooof/Noooooof/pages
+hexo clean && hexo g && hexo d && start Chrome.exe https://gitee.com/Noooooof/Noooooof/pages
