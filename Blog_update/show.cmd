ccd686e2bf109ebf34b29307da8e89b9fa235e4c
Noooooof <Black1250889060@gmail.com>
2021-08-22 14:45:34 +0800

    easy-update

--- Blog_update/show.cmd
+++
@@ -1,4 +0,0 @@
-@echo off
-E:
-cd E:\Recent\MyBlog\source\_update && python updated.py
-hexo clean && hexo g && hexo s
--- Blog_update/update.cmd
+++ Blog_update/update.cmd
@@ -2,3 +2,4 @@
 E:
 cd E:\Recent\MyBlog\source\_update && python updated.py
 hexo clean && hexo g && hexo d
+pause
