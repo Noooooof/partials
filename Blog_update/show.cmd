91507fb011437f82537937909b303d91089410cf
Noooooof <Black1250889060@gmail.com>
2021-08-22 12:44:46 +0800

    easy-update

--- Blog_update/show.cmd
+++ Blog_update/show.cmd
@@ -1,6 +1,6 @@
 @echo off
 E:
-start Chrome.exe http://localhost:4000
 cd E:\Recent\MyBlog\source\_update && python updated.py
 cd E:\Recent\MyBlog
 hexo clean && hexo g && hexo s
+start Chrome.exe http://localhost:4000
--- Blog_update/update.cmd
+++ Blog_update/update.cmd
@@ -1,5 +1,5 @@
 @echo off
 E:
 cd E:\Recent\MyBlog\source\_update && python updated.py
-cd E:\Recent\MyBlog
+E:\Recent\MyBlog
 hexo clean && hexo g && hexo d
