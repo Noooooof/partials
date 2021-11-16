737f27d4f7d5dfb7cfb56e1aca9eb6af8c80993e
Noooooof <Black1250889060@gmail.com>
2021-11-16 16:51:48 +0800

    easy-update

--- Blog_update/update.cmd
+++ Blog_update/update.cmd
@@ -1,5 +1,7 @@
 @echo off
 E:
+cd E:\Recent\MyBlog\source\_partials
+call de_git.bat
 cd E:\Recent\MyBlog\source\_update && python updated.py
-hexo clean && hexo g && hexo d
+hexo clean && hexo g && hexo s
 pause
