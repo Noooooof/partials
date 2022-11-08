b66436fc551ccb09dc793efe815c8c5d6392d51f
Noooooof <Black1250889060@gmail.com>
2022-11-08 18:13:55 +0800

    easy-update

---
+++ Function/Blog_update/show.cmd
@@ -0,0 +1,10 @@
+@echo off
+E:
+cd E:\Recent\MyBlog\source\_partials
+call de_git.bat
+color 08
+cd E:\Recent\MyBlog
+chrome  http://localhost:4000
+hexo clean && hexo g && hexo s
+chrome  http://localhost:4000
+pause>nul
